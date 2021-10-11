b7ad252484cde973320014dd5a358a735a4fc944
MikaIce <michael.benoit13@gmail.com>
2021-10-10 22:52:11 -0300

    orga + config

--- GPyBot/views.py
+++ GPyBot/views.py
@@ -2,9 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+app.config.from_object('')
+
 @app.route('/')
 def index():
     return "Hi Bebi Amandine !"
 
-if __name__ == "__main__":
-    app.run()
+#if __name__ == "__main__":
+#    app.run()
---
+++ config.py
@@ -0,0 +1,6 @@
+# To generate a new secret key:
+# >>> import random, string
+# >>> "".join([random.choice(string.printable) for _ in range(24)])
+SECRET_KEY = "d556a438ff158244c79578249d92db3f"
+
+FB_APP_ID = 1046638056142365
