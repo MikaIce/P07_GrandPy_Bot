91408ce7aa740eae636745817f49e5a9a340db5e
MikaIce <michael.benoit13@gmail.com>
2021-10-30 23:29:53 -0300

    MAJ

--- config.py
+++ config.py
@@ -1,10 +1,9 @@
 import os
 
+GOOGLE_API_KEY="AIzaSyBJBC0XAIGzQl9-As_nw5sTI2exAk0igzM"
 # Configurations for the Website
 GOOGLE_SEARCH_URL = "https://maps.googleapis.com/maps/api/place/textsearch/json"
 WIKI_SEARCH_URL = "https://fr.wikipedia.org/w/api.php"
 
-# Define the API Key in your server config 
+# Define the API Key in your server config
 GOOGLE_API_KEY = os.environ.get("GOOGLE_API_KEY")
-
-
